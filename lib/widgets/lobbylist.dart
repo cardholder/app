dea0e02b72c68532a75f2195ac8f3e695c630c9e
martistuwe <martistuwe@yahoo.de>
2019-05-23 14:56:16 +0200

    :sparkles: added communication to server

--- lib/widgets/lobbylist.dart
+++ lib/widgets/lobbylist.dart
@@ -1,11 +1,24 @@
+import 'dart:convert';
+
 import 'package:cardholder/types/Lobby.dart';
 import 'package:cardholder/types/Player.dart';
 import 'package:cardholder/widgets/ch_appbar.dart';
 import 'package:cardholder/widgets/ch_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
 class LobbyList extends StatelessWidget {
+  final channel = IOWebSocketChannel.connect("ws://ec2-18-185-18-129.eu-central-1.compute.amazonaws.com:8000/ws/chat/lobby/");
+
+  LobbyList() {
+    channel.sink.add('{"message":"Flutter ist toll"}');
+    channel.stream.listen((message) {
+      print(message);
+    });
+  }
+
   final List<Lobby> _lobbies = [
     Lobby('a', 'Skat', 'private', 6, [Player(1, 'name', 'role')])
   ];
