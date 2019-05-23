ce008d2527752f2c5620961348f2585fa205cec7
martistuwe <martistuwe@yahoo.de>
2019-05-23 18:12:15 +0200

    :sparkles: added getting lobbys from server

--- lib/widgets/lobbylist.dart
+++ lib/widgets/lobbylist.dart
@@ -1,24 +1,37 @@
 import 'package:cardholder/types/lobby.dart';
-import 'package:cardholder/types/player.dart';
 import 'package:cardholder/widgets/ch_appbar.dart';
 import 'package:cardholder/widgets/ch_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:web_socket_channel/io.dart';
+import 'dart:convert';
 
-class LobbyList extends StatelessWidget {
-  final channel = IOWebSocketChannel.connect("ws://ec2-18-185-18-129.eu-central-1.compute.amazonaws.com:8000/lobbylist/");
-  // I/flutter ( 9922): {"lobbies": [{"id": "hAsfh8n", "game": "Durak", "visibility": "private", "max_players": 8, "players": [{"id": 0, "name": "Player 1", "role": "leader"}]}]}
+class LobbyList extends StatefulWidget {
+  LobbyList({Key key}) : super(key: key);
 
-  LobbyList() {
-    channel.stream.listen((message) {
-      print(message);
-    });
+  @override
+  State<StatefulWidget> createState() {
+    return LobbyListState();
+  }
+}
+
+class LobbyListState extends State<LobbyList> {
+  final channel = IOWebSocketChannel.connect(
+      "ws://ec2-18-185-18-129.eu-central-1.compute.amazonaws.com:8000/lobbylist/");
+  List<Lobby> _lobbies = [];
+
+  LobbyListState() {
+    fetchLobbies();
   }
 
-  final List<Lobby> _lobbies = [
-    Lobby('a', 'Skat', 'private', 6, [Player(1, 'name', 'role')])
-  ];
+  Future fetchLobbies() async {
+    channel.stream.listen((message) {
+      var list = jsonDecode(message)['lobbies'] as List;
+      setState(() {
+        _lobbies = list.map((f) => Lobby.fromJson(f)).toList();
+      });
+    });
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
