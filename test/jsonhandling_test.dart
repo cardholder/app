5c3652a6b4ef4084bff9db6dc002ac14b12e69f1
martistuwe <martistuwe@yahoo.de>
2019-06-10 20:09:11 +0200

    :white_check_mark: added tests

---
+++ test/jsonhandling_test.dart
@@ -0,0 +1,42 @@
+import 'package:cardholder/types/lobby.dart';
+import 'package:cardholder/types/player.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Player:', () {
+    test('Player to Json', () {
+      var player = Player(1, 'Testname', 'leader');
+      var matcherMap = {'id': 1, 'name': 'Testname', 'role': 'leader'};
+
+      expect(player.toJson(), matcherMap);
+    });
+    test('Json to Player', () {
+      var jsonMap = {'id': 3, 'name': 'Username', 'role': 'player'};
+      var matcher = Player(3, 'Username', 'player');
+      var player = Player.fromJson(jsonMap);
+
+      expect(player.id, matcher.id);
+      expect(player.name, matcher.name);
+      expect(player.role, matcher.role);
+    });
+  });
+  group('Lobby:', () {
+    test('Lobby to Json', () {
+      var lobby = Lobby('JKhdaD', 'Skat', 'public', 6, null);
+      var matcherMap = {'id': 'JKhdaD', 'game': 'Skat', 'visibility': 'public', 'max_players': 6, 'players': null};
+
+      expect(lobby.toJson(), matcherMap);
+    });
+    test('Json to Lobby', () {
+      var jsonMap = {'id': 'DgdAbs', 'game': 'Mau-Mau', 'visibility': 'private', 'max_players': 4, 'players': null};
+      var matcher = Lobby('DgdAbs', 'Mau-Mau', 'private', 4, null);
+      var lobby = Lobby.fromJson(jsonMap);
+
+      expect(lobby.id, matcher.id);
+      expect(lobby.game, matcher.game);
+      expect(lobby.visibility, matcher.visibility);
+      expect(lobby.maxPlayers, matcher.maxPlayers);
+      expect(lobby.players, matcher.players);
+    });
+  });
+}
