e7e1ba4f6a56ef5430325125bf7a34ec7d1dc6d4
martistuwe <martistuwe@yahoo.de>
2019-06-19 22:35:37 +0200

    :construction: added cards showing their values

--- lib/widgets/playingcard.dart
+++ lib/widgets/playingcard.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+final symbols = {
+  'd': '♦',
+  'c': '♣',
+  's': '♠',
+  'h': '♥',
+};
+
 class PlayingCard extends StatelessWidget {
   final int id = 1;
   final String value = 'Q';
@@ -19,8 +26,50 @@ class PlayingCard extends StatelessWidget {
         elevation: 2,
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(4))),
-        child: Center(
-          child: Text('Card'),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 3,
+              child: Text(
+                value,
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            Positioned(
+              top: 25,
+              left: 3,
+              child: Text(
+                symbols[symbol],
+                style: TextStyle(fontSize: 13),
+              ),
+            ),
+            Center(
+              child: Text(symbols[symbol]),
+            ),
+            Positioned(
+              bottom: 7,
+              right: -14,
+              child: Container(
+                transform: Matrix4.rotationZ(3.14159),
+                child: Text(
+                  symbols[symbol],
+                  style: TextStyle(fontSize: 13),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: -30,
+              right: -13,
+              child: Container(
+                transform: Matrix4.rotationZ(3.14159),
+                child: Text(
+                  value,
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
