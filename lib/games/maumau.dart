966d74828222476aa0503832001a925f3d9298cf
martistuwe <martistuwe@yahoo.de>
2019-06-21 16:07:14 +0200

    :construction: added sections for ui elements

--- lib/games/maumau.dart
+++ lib/games/maumau.dart
@@ -15,47 +15,70 @@ class MauMauState extends State<MauMau> {
     PlayingCard(),
     PlayingCard(),
   ];
-  List<PlayingCard> pile = List();
+  List<PlayingCard> pile = [PlayingCard.undraggable(PlayingCard())];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Text('Player'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).copyWith().size.height / 3,
+          child: Row(
             children: <Widget>[
-              Stack(
+              Column(
                 children: <Widget>[
-                  PlayingCard(),
-                  PlayingCard(),
-                  PlayingCard(),
-                  PlayingCard(),
+                  Icon(Icons.polymer),
+                  Text('Name'),
                 ],
-              ),
-              DragTarget(
-                builder:
-                    (context, List<PlayingCard> candidateData, rejectedData) {
-                  return pile.length == 0 ? Container(width: 90, height: 150) : Stack(children: pile);
-                },
-                onWillAccept: (data) {
-                  return true;
-                },
-                onAccept: (data) {
-                  pile.add(data);
-                  hand.remove(data);
-                  setState(() {});
-                },
               ),
             ],
           ),
-          Row(
-            children: hand,
-          )
-        ],
-      ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                PlayingCard(),
+                PlayingCard(),
+                PlayingCard(),
+                PlayingCard(),
+              ],
+            ),
+            DragTarget(
+              builder:
+                  (context, List<PlayingCard> candidateData, rejectedData) {
+                return Stack(children: pile);
+              },
+              onWillAccept: (data) {
+                return hand.contains(data);
+              },
+              onAccept: (PlayingCard data) {
+                print('On Accept');
+                pile.add(PlayingCard.undraggable(data));
+                hand.remove(data);
+                setState(() {});
+              },
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Column(
+              children: <Widget>[
+                Row(children: hand),
+                Container(
+                  height: 40,
+                  width: MediaQuery.of(context).copyWith().size.width,
+                  color: Colors.green,
+                  child: Text('Playername'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
