d0779b2f1b257f36df5b13f27dc941c99940f4ee
martistuwe <martistuwe@yahoo.de>
2019-06-21 14:03:08 +0200

    :recycle: refactored lobbysettings

--- lib/widgets/lobbysettings.dart
+++ lib/widgets/lobbysettings.dart
@@ -33,47 +33,6 @@ class LobbySettingsState extends State<LobbySettings> {
   var visibilityOptions = ['public', 'private'];
   var scaffoldContext;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: cardholderappbar(context),
-      body: Builder(
-        builder: (BuildContext context) {
-          scaffoldContext = context;
-          return Padding(
-            padding:
-                const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 35),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    CardholderFormField(
-                        'Kartenspiel', cardgameOptions, cardgameCallback),
-                    CardholderFormField(
-                        'Spieleranzahl', maxPlayerOptions, maxPlayerCallback),
-                    CardholderFormField(
-                        'Sichtbarkeit', visibilityOptions, visibilityCallback),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Button(
-                        title: 'Lobby erstellen',
-                        onPressed: () async {
-                          _createLobby(createLobbyMsg);
-                        }),
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-
   _createLobby(Map createLobbyMsg) {
     Lobby _newLobby = Lobby.fromJson(createLobbyMsg);
     String id;
@@ -113,5 +72,46 @@ class LobbySettingsState extends State<LobbySettings> {
 
   void visibilityCallback(var option) {
     createLobbyMsg['visibility'] = option;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: cardholderappbar(context),
+      body: Builder(
+        builder: (BuildContext context) {
+          scaffoldContext = context;
+          return Padding(
+            padding:
+                const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 35),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    CardholderFormField(
+                        'Kartenspiel', cardgameOptions, cardgameCallback),
+                    CardholderFormField(
+                        'Spieleranzahl', maxPlayerOptions, maxPlayerCallback),
+                    CardholderFormField(
+                        'Sichtbarkeit', visibilityOptions, visibilityCallback),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Button(
+                        title: 'Lobby erstellen',
+                        onPressed: () async {
+                          _createLobby(createLobbyMsg);
+                        }),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
