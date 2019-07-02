0ae720443bb68c816078704c31704b57488f05d3
martistuwe <martistuwe@yahoo.de>
2019-07-02 11:08:49 +0200

    :alien: api port changed

--- lib/types/constants.dart
+++ lib/types/constants.dart
@@ -1,5 +1,5 @@
 const String url =
-    'ws://ec2-18-185-18-129.eu-central-1.compute.amazonaws.com:8000/';
+    'ws://ec2-18-185-18-129.eu-central-1.compute.amazonaws.com:80/';
 
 const List<String> cardgameOptions = ['Mau Mau'];
 const List<String> maxPlayerOptions = ['2', '3', '4', '5', '6'];
